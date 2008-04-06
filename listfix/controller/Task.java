e123114460edddcc63b1501795a338772b481d10
firewyre <firewyre@users.sourceforge.net>
2008-04-06 21:20:35 +0000

    Brining codebase into compliance with GPL license before anyone notices I didn't do it before releasing to sourceforge :)

--- listfix/controller/Task.java
+++ listfix/controller/Task.java
@@ -29,41 +29,24 @@ public abstract class Task extends Thread
     private Vector observers;
     private int progress = 0;
 
-    /** Construct a new <code>Task</code>. */
     public Task()
     {
         observers = new Vector();
     }
 
-    /** Run the task. This method is the body of the thread for this task. */
     @Override
     public abstract void run();
 
-    /** Add a progress observer to this task's list of observers. Observers are
-     * notified by the task of progress made.
-     *
-     * @param observer The observer to add.
-     */
-    public final void addProgressObserver(ProgressObserver observer)
+    public final void addProgressObserver(IProgressObserver observer)
     {
         observers.addElement(observer);
     }
 
-    /** Remove a progress observer from this task's list of observers.
-     *
-     * @param observer The observer to remove.
-     */
-    public final void removeProgressObserver(ProgressObserver observer)
+    public final void removeProgressObserver(IProgressObserver observer)
     {
         observers.removeElement(observer);
     }
 
-    /** Notify all observers about the percentage of the task completed.
-     *
-     * @param percent The percentage of the task completed, an integer value
-     * between 0 and 100 inclusive. Values outside of this range are silently
-     * clipped.
-     */
     public final void notifyObservers(int percent)
     {
         if (percent < 0)
@@ -79,7 +62,7 @@ public abstract class Task extends Thread
         Enumeration e = observers.elements();
         while (e.hasMoreElements())
         {
-            ((ProgressObserver) e.nextElement()).setProgress(percent);
+            ((IProgressObserver) e.nextElement()).setProgress(percent);
         }
     }
     
@@ -93,7 +76,7 @@ public abstract class Task extends Thread
         Enumeration e = observers.elements();
         while (e.hasMoreElements())
         {
-            ((ProgressDialog)e.nextElement()).setMessage(message);
+            ((ProgressPopup)e.nextElement()).setMessage(message);
         }
     }
 }
