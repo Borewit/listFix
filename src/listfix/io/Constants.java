59f733c57d0882faef86bae8d4cd902bfdb25ee5
firewyre <firewyre@users.sourceforge.net>
2012-09-02 20:58:02 +0000

    Test 1 in the new repo...

--- src/listfix/io/Constants.java
+++ src/listfix/io/Constants.java
@@ -1,7 +1,7 @@
 /*
  *  listFix() - Fix Broken Playlists!
  *  Copyright (C) 2001-2012 Jeremy Caron
- * 
+ *  
  *  This file is part of listFix().
  * 
  *  This program is free software; you can redistribute it and/or
