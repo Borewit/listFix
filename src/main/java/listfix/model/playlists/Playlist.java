a403433b378003fadb7b64dcaf206f9078cb050d
Borewit <Borewit@noreply.github.com>
2023-02-04 17:20:46 +0100

    Fix closest-match-progress

--- src/main/java/listfix/model/playlists/Playlist.java
+++ src/main/java/listfix/model/playlists/Playlist.java
@@ -883,7 +883,7 @@ public class Playlist
   public List<BatchMatchItem> findClosestMatches(List<PlaylistEntry> entries, Collection<String> libraryFiles, IProgressObserver<String> observer)
   {
     ProgressAdapter<String> progress = ProgressAdapter.wrap(observer);
-    progress.setTotal(entries.size() * libraryFiles.size());
+    progress.setTotal(entries.size());
 
     List<BatchMatchItem> fixed = new LinkedList<>();
     int ix = 0;
@@ -892,13 +892,14 @@ public class Playlist
       if (observer.getCancelled()) return null;
       if (!entry.isURL() && !entry.isFound())
       {
-        List<PotentialPlaylistEntryMatch> matches = entry.findClosestMatches(libraryFiles, progress, this.playListOptions);
+        List<PotentialPlaylistEntryMatch> matches = entry.findClosestMatches(libraryFiles, null, this.playListOptions);
         if (!matches.isEmpty())
         {
           fixed.add(new BatchMatchItem(ix, entry, matches));
         }
       }
       ix++;
+      progress.stepCompleted();
     }
 
     return fixed;
