641a12fc3f8cdd8326acd1e0d91b2aeeda9db2d4
Borewit <Borewit@users.noreply.github.com>
2023-02-11 19:12:27 +0100

    Add unit tests for reading WPL playlists

---
+++ src/test/java/listfix/io/playlists/wpl/WPLReaderTests.java
@@ -0,0 +1,55 @@
+package listfix.io.playlists.wpl;
+
+import listfix.io.IPlaylistOptions;
+import listfix.json.JsonAppOptions;
+import listfix.model.playlists.PlaylistEntry;
+import listfix.util.TestUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class WPLReaderTests
+{
+  private IPlaylistOptions playlistOptions;
+
+  @Before
+  public void initOptions() {
+    this.playlistOptions = new JsonAppOptions();
+  }
+
+  private WPLReader buildWplReader(String playlist) throws IOException
+  {
+    File m3uPlaylistFile = TestUtil.createFileFromResource(this, "/playlists/wpl/" + playlist);
+    return new WPLReader(this.playlistOptions, m3uPlaylistFile.toPath());
+  }
+
+  @Test
+  @DisplayName("Read WPL Playlist: 2seq.wpl")
+  public void read_2seq() throws IOException
+  {
+    WPLReader wplReader = buildWplReader("2seq.wpl");
+    List<PlaylistEntry> wplPlaylist = wplReader.readPlaylist();
+    assertNotNull(wplPlaylist, "PlaylistFactory should read and construct M3U playlist");
+    assertEquals(4, wplPlaylist.size(), "M3U playlist contains 2 tracks");
+    assertEquals(StandardCharsets.UTF_8, wplReader.getEncoding());
+  }
+
+  @Test
+  @DisplayName("Read WPL Playlist: playlist.wpl")
+  public void read_playlist() throws IOException
+  {
+    WPLReader wplReader = buildWplReader("playlist.wpl");
+    List<PlaylistEntry> wplPlaylist = wplReader.readPlaylist();
+    assertNotNull(wplPlaylist, "PlaylistFactory should read and construct M3U playlist");
+    assertEquals(2, wplPlaylist.size(), "M3U playlist contains 2 tracks");
+    assertEquals(StandardCharsets.UTF_8, wplReader.getEncoding());
+  }
+}
