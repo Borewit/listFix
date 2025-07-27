52bdd5e3286ba86a19c49150920b5daf85af6c8c
excelsior <44299346+excelsior-dev@users.noreply.github.com>
2025-07-27 17:43:02 +0200

    add path sanitizer test for windows absolute path

--- src/test/java/listfix/util/PathSanitizerTests.java
+++ src/test/java/listfix/util/PathSanitizerTests.java
@@ -1,6 +1,7 @@
 package listfix.util;
 
 import java.nio.file.Path;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,10 +17,18 @@ public class PathSanitizerTests
   {
 
     // Windows specific
-    Path path = PathSanitizer.makePath("..\\Dillon\\2011 - This Silence Kills\\03. Thirteen ? Thirtyfive.flac");
-    assertNotNull(path);
-    assertTrue(path.getFileName().toString().startsWith("03. Thirteen "));
-    assertNotNull(path.getParent(), "the file should have a parent path");
-    assertEquals(path.getParent().getFileName().toString(), "2011 - This Silence Kills");
+    List<String> pathStrings = List.of(
+                "..\\Dillon\\2011 - This Silence Kills\\03. Thirteen ? Thirtyfive.flac",    // Relative path
+                "D:\\Dillon\\2011 - This Silence Kills\\03. Thirteen ? Thirtyfive.flac",    // Absolute path
+                "D:Dillon\\2011 - This Silence Kills\\03. Thirteen ? Thirtyfive.flac"       // Relative path from the current directory of the D: drive
+    );
+
+    for (String pathStr : pathStrings) {
+        Path path = PathSanitizer.makePath(pathStr);
+        assertNotNull(path);
+        assertTrue(path.getFileName().toString().startsWith("03. Thirteen "));
+        assertNotNull(path.getParent(), "the file should have a parent path");
+        assertEquals(path.getParent().getFileName().toString(), "2011 - This Silence Kills");
+    }
   }
 }
